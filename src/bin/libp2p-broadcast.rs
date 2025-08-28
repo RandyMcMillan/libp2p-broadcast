377e1cf60b70009a7526b4fc1cc846ed5fc331ce
randymcmillan <randymcmillan@protonmail.com>
2025-08-28 16:26:03 -0400

    feat: Implement clap for command-line argument parsing and cleanup

--- src/bin/libp2p-broadcast.rs
+++ src/bin/libp2p-broadcast.rs
@@ -1,3 +1,4 @@
+use clap::Parser;
 use libp2p_broadcast::protocol::Message;
 use libp2p_broadcast::{Broadcast, BroadcastConfig, Topic /*, BroadcastEvent*/};
 use log::debug;
@@ -7,25 +8,33 @@ use log::debug;
 //use std::io::{Error, ErrorKind, Result};
 use std::sync::Arc;
 
+/// Simple program to broadcast messages
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    /// Topic to subscribe to and broadcast on
+    #[arg(short, long, default_value = "my-default-topic")]
+    topic: String,
+}
+
 fn main() {
+    let cli = Cli::parse();
+
     debug!("Hello from libp2p-broadcast binary!");
 
-    let mut topic = Topic::new(b"my-topic-1");
+    let topic = Topic::new(cli.topic.as_bytes());
 
     let msgs = [
         Message::Broadcast(Topic::new(b""), Arc::new(*b"")),
-        Message::Subscribe(topic),
-        Message::Unknown(),
-        Message::Unsubscribe(topic),
-        Message::Broadcast(topic, Arc::new(*b"content")),
+        Message::Subscribe(topic.clone()),
+        Message::Unsubscribe(topic.clone()),
+        Message::Broadcast(topic.clone(), Arc::new(*b"content")),
     ];
     for msg in &msgs {
         println!("msg: {:?}", msg);
-        let msg2 = Message::from_bytes(&msg.to_bytes()).unwrap();
-        //assert_eq!(msg, &msg2);
+        let _msg2 = Message::from_bytes(&msg.to_bytes()).unwrap();
     }
 
-    topic = Topic::new(b"my-topic-2");
     let config = BroadcastConfig::default();
     let mut broadcast = Broadcast::new(config);
     broadcast.subscribe(topic.clone());
@@ -34,8 +43,8 @@ fn main() {
     let msgs = [
         Message::Broadcast(Topic::new(b""), Arc::new(*b"")),
         Message::Subscribe(Topic::new(b"")),
-        Message::Subscribe(topic),
-        Message::Unsubscribe(topic),
+        Message::Subscribe(topic.clone()),
+        Message::Unsubscribe(topic.clone()),
         Message::Broadcast(topic, Arc::new(*b"content")),
     ];
     for msg in &msgs {
